e058ddce920e4da3566a8560782f79e0659e318a
Ted DDDAO <ted.dddao@gmail.com>
2022-01-21 08:43:39 -0800

    update DigitalDustDAO tests, remove (broken) tests around ERC11555WithAccess

--- test/DigitalDustDAO.test.js
+++ test/DigitalDustDAO.test.js
@@ -1,8 +1,9 @@
-const { expectRevert } = require('@openzeppelin/test-helpers');
+const { BN, expectEvent, expectRevert } = require('@openzeppelin/test-helpers');
+
 const DigitalDustDAO = artifacts.require("DigitalDustDAO");
 
-contract('DigitalDustDAO', accounts => {
-    let instance;
+contract('DigitalDustDAO', (accounts) => {
+    let contract;
     const [creator, userA, userB, ...others] = accounts;
     const RIGHTS = {
         none: 0,
@@ -13,80 +14,119 @@ contract('DigitalDustDAO', accounts => {
     };
 
     before(async () => {
-        instance = await DigitalDustDAO.deployed();
+        contract = await DigitalDustDAO.new({ from: creator });
     });
 
-    it('should assign rights to creator', async () => {
-        const currentRights = await instance.rightsOf(0, creator);
-        expect(currentRights.toNumber()).to.equal(1000);
+    it('Constructor should assign rights to creator', async () => {
+        const response = await contract.rightsOf(0, creator);
+        expect(response.toNumber()).to.equal(1000);
+    });
+
+    it.skip('should emit SetRights', async () => {
+        await expectEvent.inConstruction(contract, 'SetRights', {
+            id: new BN(0),
+            from: new BN(0),
+            to: creator,
+            rights: new BN(1000)
+        });
     });
 
     it('should not contain rights for userA', async () => {
-        const currentRights = await instance.rightsOf(0, userA);
-        expect(currentRights.toNumber()).to.equal(RIGHTS.none);
+        const response = await contract.rightsOf(0, userA);
+        expect(response.toNumber()).to.equal(RIGHTS.none);
     });
 
     it('userA should not be allowed to increase their own rights', async () => {
         await expectRevert(
-            instance.setRights(0, userA, RIGHTS.revoke, { from: userA }),
+            contract.setRights(0, userA, RIGHTS.revoke, { from: userA }),
             'Not enough rights to grant rights'
         );
     });
 
     it('userA should not be allowed to grant rights to userB', async () => {
         await expectRevert(
-            instance.setRights(0, userB, RIGHTS.grant, { from: userA }),
+            contract.setRights(0, userB, RIGHTS.grant, { from: userA }),
             'Not enough rights to grant rights'
         );
     });
 
-    it('creator should grant rights to userA', async () => {
-        await instance.setRights(0, userA, RIGHTS.grant);
-        const currentRights = await instance.rightsOf(0, userA);
+    it('creator should grant rights to userA, and emit SetRights', async () => {
+        const receipt = await contract.setRights(0, userA, RIGHTS.grant);
+
+        const initialRights = await contract.rightsOf(0, userA);
+        expect(initialRights.toNumber()).to.equal(RIGHTS.grant);
+
+        expectEvent(receipt, 'SetRights', {
+            id: new BN(0),
+            from: creator,
+            to: userA,
+            rights: new BN(RIGHTS.grant)
+        });
+
+        const currentRights = await contract.rightsOf(0, userA);
         expect(currentRights.toNumber()).to.equal(RIGHTS.grant);
     });
 
-    it('userA should have grant rights', async () => {
-        const currentRights = await instance.rightsOf(0, userA);
-        expect(currentRights.toNumber()).to.equal(RIGHTS.grant);
-    });
+    it('userA should grant rights to userB and emit SetRights', async () => {
+        const receipt = await contract.setRights(0, userB, RIGHTS.grant, { from: userA });
 
-    it('userA should grant rights to userB', async () => {
-        await instance.setRights(0, userB, RIGHTS.grant, { from: userA });
-        const currentRights = await instance.rightsOf(0, userB);
+        expectEvent(receipt, 'SetRights', {
+            id: new BN(0),
+            from: userA,
+            to: userB,
+            rights: new BN(RIGHTS.grant)
+        });
+
+        const currentRights = await contract.rightsOf(0, userB);
         expect(currentRights.toNumber()).to.equal(RIGHTS.grant);
     });
 
     it('userA should not be allowed to grant more rights than they have', async () => {
         await expectRevert(
-            instance.setRights(0, userB, RIGHTS.revoke, { from: userA }),
+            contract.setRights(0, userB, RIGHTS.revoke, { from: userA }),
             'Callers rights cannot exceed granted rights'
         );
     });
 
     it('userA should not be allowed to revoke rights', async () => {
         await expectRevert(
-            instance.setRights(0, userB, RIGHTS.grant, { from: userA }),
+            contract.setRights(0, userB, RIGHTS.grant, { from: userA }),
             'Not enough rights to revoke rights'
         );
     });
 
     it('creator should grant revoke rights to userA', async () => {
-        await instance.setRights(0, userA, RIGHTS.revoke, { from: creator });
-        const currentRights = await instance.rightsOf(0, userA);
+        const receipt = await contract.setRights(0, userA, RIGHTS.revoke, { from: creator });
+
+        const currentRights = await contract.rightsOf(0, userA);
         expect(currentRights.toNumber()).to.equal(RIGHTS.revoke);
+
+        expectEvent(receipt, 'SetRights', {
+            id: new BN(0),
+            from: creator,
+            to: userA,
+            rights: new BN(RIGHTS.revoke)
+        });
     });
 
     it('userA should be able to revoke rights from userB', async () => {
-        await instance.setRights(0, userB, RIGHTS.none, { from: userA });
-        const currentRights = await instance.rightsOf(0, userB);
+        const receipt = await contract.setRights(0, userB, RIGHTS.none, { from: userA });
+
+        const currentRights = await contract.rightsOf(0, userB);
         expect(currentRights.toNumber()).to.equal(RIGHTS.none);
+
+        expectEvent(receipt, 'SetRights', {
+            id: new BN(0),
+            from: userA,
+            to: userB,
+            rights: new BN(RIGHTS.none)
+        });
     });
 
     it('userA should not be allowed to revoke rights from someone with higher rights', async () => {
         await expectRevert(
-            instance.setRights(0, creator, RIGHTS.none, { from: userA }),
-            'Caller cannot revoke rights from elevated member'
+            contract.setRights(0, creator, RIGHTS.none, { from: userA }),
+            'Cannot revoke rights from higher ranked accounts'
         );
     });
 });
