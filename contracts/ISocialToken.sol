65c09e73b3958d2ab3631117c01988f11e43fd5f
Teal <thatablaze@gmail.com>
2022-01-22 23:04:04 -0800

    Wrote the unstake function.

--- contracts/ISocialToken.sol
+++ contracts/ISocialToken.sol
@@ -3,5 +3,18 @@
 pragma solidity 0.8.11;
 
 interface ISocialToken {
-    
+    event Staked (
+        address indexed account,
+        uint16 indexed duration,
+        uint64 indexed endDay,
+        uint256 amount,
+        uint32 interestRate,
+        uint96 id
+    );
+
+    event RedeemedStake (
+        address indexed account,
+        uint256 principal,
+        int256 interest
+    );
 }
--- contracts/LongTailSocialToken.sol
+++ contracts/LongTailSocialToken.sol
@@ -18,7 +18,7 @@ contract LongTailSocialToken is ISocialToken, ERC777 {
     struct StakeData {
         uint64 start;
         uint64 end;
-        uint32 interest;
+        uint32 interestRate;
         uint96 index;
         uint256 principal;
     }
@@ -26,6 +26,7 @@ contract LongTailSocialToken is ISocialToken, ERC777 {
     //uint constant STAKE_ARRAY_SIZE = 5872;
     uint private constant MAXIMUM_STAKE_DAYS = 5840;
     uint private constant MININUM_STAKE_DAYS = 30;
+    uint private constant MININUM_STAKE_AMOUNT = 100000000000;
     //uint constant MAXIMUM_STAKES_PER_ADDRESS = uint24.max;
 
     mapping(uint64 => StakeDataPointer[]) private stakesByEndDay;
@@ -57,7 +58,7 @@ contract LongTailSocialToken is ISocialToken, ERC777 {
         nftContract = ISocialTokenNFT(newNFT);
     }
 
-    function stake(uint256 amount, uint64 numberOfDays) public returns(uint256) {
+    function stake(uint256 amount, uint16 numberOfDays) public returns(uint96) {
         // cache refrence variables
         address stakeAccount = _msgSender();
         uint64 currentDay = _currentDay();
@@ -66,13 +67,14 @@ contract LongTailSocialToken is ISocialToken, ERC777 {
         uint256 endDayIndex = stakesByEndDay[endDay].length;
 
         // ensure inputs are not out of range 
+        require(amount >= MININUM_STAKE_AMOUNT, "Amount of stake too low");
         require(balanceOf(stakeAccount) >= amount, "Insufficient balance");
         require(numberOfDays <= MAXIMUM_STAKE_DAYS, "Stake duration is too long"); 
-        require(numberOfDays >= MININUM_STAKE_DAYS, "Stake was too short");
-        require(accountIndex <= type(uint96).max, "This address may no longer stake");
+        require(numberOfDays >= MININUM_STAKE_DAYS, "Stake duration is too short");
+        require(accountIndex <= type(uint96).max, "Maximum number of stakes reached for this address");
         require(endDayIndex <= type(uint96).max, "Too many stakes are ending on that day");
 
-        //populate stake data
+        // populate stake data
         stakesByEndDay[endDay].push(StakeDataPointer(
             stakeAccount,
             uint96(accountIndex)
@@ -81,21 +83,50 @@ contract LongTailSocialToken is ISocialToken, ERC777 {
         stakesByAccount[stakeAccount].push(StakeData(
             currentDay, 
             endDay,
-            _calculateInterest(numberOfDays), 
+            _calculateInterestRate(numberOfDays), 
             uint96(endDayIndex),
             amount
         ));
 
         // send 
-        _send(stakeAccount, address(this), amount, "", "staked", false);
+        _send(stakeAccount, address(this), amount, "", "", true);
 
-        //TODO: emit event
+        emit Staked(stakeAccount, numberOfDays, endDay, amount, stakesByAccount[stakeAccount][accountIndex].interestRate, uint96(accountIndex));
 
-        return accountIndex;
+        return uint96(accountIndex);
     }
 
-    function unstake(uint256 stakeNumber) public {
+    function unstake(uint96 stakeNumber) public {
+        // cache refrence variables
+        address stakeAccount = _msgSender();
+        uint256 principal = stakesByAccount[stakeAccount][stakeNumber].principal;
+
+        // ensure outputs within range 
+        require(principal > 0, "Stake does not exist or has already been redeemed");
         
+        // calculate the reward
+        (bool positive, uint256 interest) = _calculateInterest(
+            stakesByAccount[stakeAccount][stakeNumber].start,
+            stakesByAccount[stakeAccount][stakeNumber].end,
+            stakesByAccount[stakeAccount][stakeNumber].interestRate,
+            stakesByAccount[stakeAccount][stakeNumber].principal);
+
+        // delete the stake data
+        delete(stakesByEndDay[stakesByAccount[stakeAccount][stakeNumber].end][stakesByAccount[stakeAccount][stakeNumber].index]);
+        delete(stakesByAccount[stakeAccount][stakeNumber]);
+
+        // distribute the funds
+        if (positive) {
+            _send(address(this), stakeAccount, principal, "", "", true);
+            _mint(stakeAccount, interest, "", "staking reward");
+        }
+        else {
+            _send(address(this), stakeAccount, principal - interest, "", "", true);
+            _burn(address(this), interest, "", "penality for early withdrawal");
+        }
+
+        // emit events
+        emit RedeemedStake(stakeAccount, principal, positive ? int256(interest) : (int256(interest) * -1));
     }
 
     function getStakeStart (address account, uint id) public view returns(uint64) {
@@ -106,8 +137,8 @@ contract LongTailSocialToken is ISocialToken, ERC777 {
         return stakesByAccount[account][id].end;
     }
 
-    function getStakeInterest (address account, uint id) public view returns(uint32) {
-        return stakesByAccount[account][id].interest;
+    function getStakeInterestRate (address account, uint id) public view returns(uint32) {
+        return stakesByAccount[account][id].interestRate;
     }
 
     function getStakePrincipal (address account, uint id) public view returns(uint256) {
@@ -118,7 +149,11 @@ contract LongTailSocialToken is ISocialToken, ERC777 {
         
     }
 
-    function _calculateInterest(uint64 numberOfDays) private returns(uint32) {
+    function _calculateInterest(uint64 start, uint64 end, uint32 interestRate, uint256 principal) private pure returns(bool, uint256) {
+
+    }
+
+    function _calculateInterestRate(uint64 numberOfDays) private pure returns(uint32) {
         
     }
 }
