77d69b7308979fbce2850d8252ba10595de0c2a8
Ted DDDAO <ted.dddao@gmail.com>
2022-02-08 08:57:44 -0800

    update LTST tests around constructor, initialized values

--- test/SocialToken/LongTailSocialToken.test.js
+++ test/SocialToken/LongTailSocialToken.test.js
@@ -1,11 +1,11 @@
 const { BN, expectEvent, expectRevert } = require('@openzeppelin/test-helpers');
 
-const DigitalDustDAO = artifacts.require('DigitalDustDAO');
-const SocialTokenManager = artifacts.require('SocialTokenManager');
+const DigitalDustDAOMock = artifacts.require('DigitalDustDAOMock');
+const SocialTokenManagerMock = artifacts.require('SocialTokenManagerMock');
 const ISocialTokenManager = artifacts.require('ISocialTokenManager');
-const LongTailSocialToken = artifacts.require('LongTailSocialToken');
+const LongTailSocialTokenMock = artifacts.require('LongTailSocialTokenMock');
 
-contract.skip('LongTailSocialToken', (accounts) => {
+contract('LongTailSocialToken', (accounts) => {
     const zeroAddress = '0x0000000000000000000000000000000000000000';
     const tokenAddress = '0x0000000000000000000000000000000000000123';
     const nftAddress = '0x0000000000000000000000000000000000000321';
@@ -21,12 +21,99 @@ contract.skip('LongTailSocialToken', (accounts) => {
     };
 
     before(async () => {
-        DAO = await DigitalDustDAO.deployed();
-        STM = await SocialTokenManager.deployed();
-        LTST = await LongTailSocialToken.deployed();
+        DAO = await DigitalDustDAOMock.deployed();
+        STM = await SocialTokenManagerMock.deployed();
+        LTST = await LongTailSocialTokenMock.deployed();
     });
 
-    it('initializes LTST', async () => {
-        expectEvent(LTST).to.be.defined;
+    
+    describe('constructor', () => {
+        it('sets the manager address', async () => {
+            const managerAddress = await LTST.getManager();
+            expect(managerAddress).to.equal(STM.address);
+        });
+
+        it('sets lastInterestAdjustment to uint64.max', async () => {
+            const lastInterestAdjustment = await LTST.getLastInterestAdjustment();
+            const maxUint64 = '18446744073709551615';  // 2**64 - 1
+            expect(lastInterestAdjustment.toString()).to.equal(maxUint64);
+        });
+
+        describe('Should set initial rates', () => {
+            let baseInterestRate,
+                linearInterestBonus,
+                quadraticInterestBonus,
+                rewardPerMiningTask,
+                miningGasReserve;
+
+            before(async () => {
+                const rates = await LTST.getContractInterestRates();
+                ([
+                    baseInterestRate,
+                    linearInterestBonus,
+                    quadraticInterestBonus,
+                    rewardPerMiningTask,
+                    miningGasReserve
+                ] = Object.values(rates).map(n => n.toNumber()));
+            });
+
+            it('Should set baseInterestRate', () => {
+                expect(baseInterestRate).to.equal(50);
+            });
+
+            it('Should set linearInterestBonus', () => {
+                expect(linearInterestBonus).to.equal(25);
+            });
+
+            it('Should set quadraticInterestBonus', () => {
+                expect(quadraticInterestBonus).to.equal(10);
+            });
+
+            it('Should set rewardPerMiningTask', () => {
+                expect(rewardPerMiningTask).to.equal(50);
+            });
+
+            it('Should set miningGasReserve', () => {
+                expect(miningGasReserve).to.equal(1500);
+            });
+        });
     });
+
+    describe.only('setManager', () => {
+        it('Should deny access from userA', async () => {
+            await expectRevert.unspecified(
+                LTST.setManager(zeroAddress, true, { from: userA })
+            );
+        });
+
+        it('Should deny access from creator', async () => {
+            await expectRevert.unspecified(
+                LTST.setManager(zeroAddress, true, { from: creator })
+            );
+        });
+
+        it('Should allow access from manager contract', async () => {
+            const newManager = await SocialTokenManagerMock.new(DAO.address, 9999, { from: creator });
+            await LTST.setMsgSender(STM.address)
+            await LTST.setManager(newManager.address, true);
+            expect(await LTST.getManager()).to.equal(newManager.address);
+        });
+    });
+
+    describe('setInterestRates', () => {});
+    describe('stake', () => {});
+    describe('unstake', () => {});
+    describe.skip('mine', () => {});
+    describe('forge', () => {});
+    describe('getNumMiningTasks', () => {});
+    describe('getContractInterestRates', () => {});
+    describe('getStakeValues', () => {});
+    describe('getCurrentDay', () => {});
+    describe('getVotingPower', () => {});
+    describe('calculateInterest', () => {});
+    describe('_votingWeight', () => {});
+    describe('_fullInterest', () => {});
+    describe('calculateInterestRate', () => {});
+    describe('transfer', () => {});
+    describe('send', () => {});
 });
