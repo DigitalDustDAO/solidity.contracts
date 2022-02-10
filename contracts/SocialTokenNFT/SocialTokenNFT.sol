fe34364c167980b5870f2938d37a217d6b0f396b
Teal <thatablaze@gmail.com>
2022-02-10 00:56:51 -0800

    Added most of the functions needed for the NFT contract.

--- contracts/SocialTokenNFT/SocialTokenNFT.sol
+++ contracts/SocialTokenNFT/SocialTokenNFT.sol
@@ -15,15 +15,32 @@ contract LongTailSocialNFT is ISocialTokenNFT, ERC721 {
     string public baseTokenURI;
 
     struct NFTData {
-        uint16 level,
-        uint32 group,
-        uint128 index
+        uint64 level;
+        uint64 group;
+        uint128 index;
     }
 
-    
+    uint private constant MAXIMUM_LEVEL = 8;
+
+    mapping(uint256 => NFTData) private dataMap;
+    mapping(address => uint256[MAXIMUM_LEVEL]) private levelBalances;
+
+    uint128[MAXIMUM_LEVEL][] private definedGroups;
+    uint128[MAXIMUM_LEVEL][] private nextIndex;
+    uint128 private elementSize;
+    uint128 private nextElementIndex;
+    uint256 private nextTokenId;
+    uint256 private maximumElementMint;
+    uint256 private elementMintCost;
+    uint64[MAXIMUM_LEVEL] private interestBonuses;
 
     constructor(address manager_) ERC721("Long Tail Social NFT", "LTSNFT") {
         manager = ISocialTokenManager(manager_);
+
+        interestBonuses[0] = 32768;
+        for(uint256 i = 1;i < MAXIMUM_LEVEL;i++) {
+            interestBonuses[i] = interestBonuses[i - 1] * 2;
+        }
     }
 
     /**
@@ -35,8 +52,22 @@ contract LongTailSocialNFT is ISocialTokenNFT, ERC721 {
             || super.supportsInterface(interfaceId);
     }
 
+    function setInterestBonus(uint256 level, uint64 newBonus) external {
+        manager.authorize(_msgSender(), ISocialTokenManager.Sensitivity.Elder);
+        require(level <= MAXIMUM_LEVEL);
+
+        interestBonuses[level] = newBonus;
+    }
+
     function interestBonus(address account) external view returns(uint64) {
-        return 0;
+        int256 maxLevel = int(MAXIMUM_LEVEL);
+        unchecked {
+            while(maxLevel >= 0 && levelBalances[account][uint256(maxLevel)] == 0) {
+                maxLevel--;
+            }
+
+            return maxLevel >= 0 ? interestBonuses[uint256(maxLevel)] : 0;
+        }
     }
 
     function setManager(address newManager) external {
@@ -53,5 +84,69 @@ contract LongTailSocialNFT is ISocialTokenNFT, ERC721 {
         baseTokenURI = newURI;
     }
 
-    function forge(uint256 quantity)
+    function setElementForgeValues(uint256 newMax, uint256 newCost) public {
+        manager.authorize(_msgSender(), ISocialTokenManager.Sensitivity.Maintainance);
+
+        maximumElementMint = newMax;
+        elementMintCost = newCost;
+    }
+
+    function forgeElements() public {
+        forgeElements(1);
+    }
+
+    function forgeElements(uint256 quantity) public {
+        require(quantity <= maximumElementMint);
+        require(quantity > 0);
+
+        manager.getTokenContract().forge(_msgSender(), int256(quantity * elementMintCost));
+
+        for (uint256 i = 0;i < quantity;i++) {
+
+            _safeMint(_msgSender(), nextTokenId);
+            dataMap[nextTokenId] = NFTData (0, 0, nextElementIndex);
+            levelBalances[_msgSender()][0]++;
+
+            nextElementIndex = (nextElementIndex + 1) % elementSize;
+        }
+    }
+
+    //TODO: forge
+
+    function resizeLibarary(uint64 group, uint128[] memory sizes) public {
+        manager.authorize(_msgSender(), ISocialTokenManager.Sensitivity.Maintainance);
+
+        // Expand the array (if nessessary)
+        // TODO: Need to check if I'm using arrays correctly here
+        for (uint256 i = 0;i < sizes.length;i++) {
+            while (definedGroups[i].length < group ) {
+                definedGroups.push([0]);
+            }
+
+            definedGroups[i][group] = sizes[i];
+        }
+    }
+
+    function resizeElementLibarary(uint128 size) public {
+        manager.authorize(_msgSender(), ISocialTokenManager.Sensitivity.Maintainance);
+
+        elementSize = size;
+        if (nextElementIndex > size) {
+            nextElementIndex = 0;
+        }
+    }
+
+    function getGroupSizes(uint64 group) public view returns(uint128[MAXIMUM_LEVEL] memory) {
+        uint128[MAXIMUM_LEVEL] memory sizes;
+        uint256 size = 0;
+        while (definedGroups[size].length >= group && size < MAXIMUM_LEVEL) {
+            size++;
+        }
+
+        for (uint256 i = 0;i < size;i++) {
+            sizes[i] = definedGroups[i][group];
+        }
+
+        return sizes;
+    }
 }
