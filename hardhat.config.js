f07bc6248df7f5e4b8f408385ae38da96c0a2bf6
Ted DDDAO <ted.dddao@gmail.com>
2022-02-16 07:39:14 -0800

    add infra for using hardhat

---
+++ hardhat.config.js
@@ -0,0 +1,19 @@
+const { task } = require("hardhat/config");
+
+require("@nomiclabs/hardhat-truffle5");
+require("@nomiclabs/hardhat-web3");
+require("hardhat-contract-sizer");
+
+task("accounts", "Prints the list of accounts", async () => {
+    const accounts = await ethers.getSigners();
+    for (const account of accounts) {
+        console.log(account.address);
+    }
+});
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+    solidity: "0.8.11"
+};
