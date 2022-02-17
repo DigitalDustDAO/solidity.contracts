ba46b4e92e1ea38b696055ff16123ec7cc2ffdd1
Ted DDDAO <ted.dddao@gmail.com>
2022-02-17 14:13:44 -0800

    add solidity optimization w/10000 runs

--- hardhat.config.js
+++ hardhat.config.js
@@ -16,5 +16,13 @@ task("accounts", "Prints the list of accounts", async () => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-    solidity: "0.8.11"
+    solidity: {
+        version: "0.8.11",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 10000,
+            },
+        },
+    }
 };
